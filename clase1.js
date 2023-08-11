63e6885ce3b1e875e3712002e1d709aa9ed1c68a
Cenizinho <136343734+Cenizinho@users.noreply.github.com>
2023-08-11 08:35:33 -0300

    Tp Javascript

---
+++ clase1.js
@@ -0,0 +1,32 @@
+alert ('El resultado de la suma es ' + (40+2))
+alert ('El resultado de la multiplicacion es ' + (40*2))
+alert ('El resultado de la division es ' + (40/2))
+alert ('El resultado de la resta es ' + (40-2))
+alert('Mi edad es ' + (2023-2009))
+
+console.log(40+2)
+console.log(40*2)
+console.log(40/2)
+console.log(40-2)
+
+let NombreAlumnoApellidoAlumno
+
+NombreAlumnoApellidoAlumno = prompt('Ingresa tu nombre y apellido ')
+
+alert('Bienvenido ' + NombreAlumnoApellidoAlumno)
+
+console.log (NombreAlumnoApellidoAlumno)
+
+let FechaNacimiento
+
+FechaNacimiento = prompt('Ingresa tu Nacimiento')
+
+alert('Tu edad es ' + (2023 - FechaNacimiento))
+
+let diez = 10
+
+let tres = 3
+
+let ResultadoMultiplicacion = diez * tres
+  
+alert(ResultadoMultiplicacion)
